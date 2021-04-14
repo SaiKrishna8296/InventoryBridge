97c3da56a64cb1e2b97ad225d77134ac3413ca20
SaiKrishna8296 <82498731+SaiKrishna8296@users.noreply.github.com>
2021-04-14 16:44:16 +0530

    Add files via upload

---
+++ Service/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ascend.MUService.Service.Controllers
+{
+    
+    public abstract class BaseController : Controller
+    {
+        protected readonly ILogger<BaseController> logger;
+        public IConfiguration Configuration { get; }
+        public int? UserID { get; set; }
+        public string JWTToken { get; set; }
+        public string SessionKey { get; set; }
+
+        protected BaseController(IConfiguration configuration, ILogger<BaseController> log)
+        {
+            Configuration = configuration;
+            logger = log;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (this.User != null)
+            {
+                
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ Service/Controllers/InventoryController.cs
@@ -0,0 +1,99 @@
+﻿using Api.Interface;
+using Ascend.MUService.Service.Controllers;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampleproject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : BaseController
+    {
+        public readonly IInventoryservice _IInventoryservice;
+        public InventoryController(
+            IConfiguration configuration,
+            ILogger<BaseController> logger,
+            IInventoryservice inventoryservice) : base(configuration, logger)
+        {
+            _IInventoryservice = inventoryservice;
+        }
+        [HttpGet]
+        [Route("Items")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var ItemsList = await _IInventoryservice.GetItems();
+                return Ok(ItemsList);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+        }
+
+        [HttpPost]
+        [Route("createItem")]
+        public async Task<IActionResult> AddItem([FromBody] InventoryWithOutId inventory)
+        {
+            try
+            {
+                var ItemsList = await _IInventoryservice.CreateItem(inventory);
+                return Ok(ItemsList);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+        }
+        [HttpPut]
+        [Route("updateItem/{id}")]
+        public async Task<IActionResult> UpdateItem(InventoryWithOutId inventory, int id)
+        {
+
+            try
+            {
+                var result = await _IInventoryservice.UpdateItem(inventory, id);
+                return Ok(result);
+            }
+
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+
+        }
+        [HttpDelete]
+        [Route("deleteItem")]
+        public async Task<IActionResult> DeleteItem( int id)
+        {
+
+            try
+            {
+                var result = await _IInventoryservice.DeleteItem(id);
+                return Ok(result);
+            }
+
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+
+        }
+    }
+
+}
